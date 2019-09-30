81fa5ce86bc7599ee3b246adcc38bf572e6afc45
Niveathika <niveathika@wso2.com>
2019-09-30 21:10:20 +0530

    Make parameter dynamic

--- component/src/main/java/io/siddhi/extension/execution/rdbms/QueryStreamProcessor.java
+++ component/src/main/java/io/siddhi/extension/execution/rdbms/QueryStreamProcessor.java
@@ -82,6 +82,7 @@ import java.util.stream.Collectors;
                                 "can be passed to set the values of the parameters",
                         type = {DataType.STRING, DataType.BOOL, DataType.INT, DataType.DOUBLE, DataType.FLOAT,
                                 DataType.LONG},
+                        dynamic = true,
                         optional = true,
                         defaultValue = "<Empty_String>"
                 ),
