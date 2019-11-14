06ad48ad7bcd9093e81dd3f07730b1a75499d66a
Niveathika <niveathika@wso2.com>
2019-11-14 15:21:10 +0530

    Migrate osgi test distribution to 5.1.0

--- tests/osgi-tests/src/test/java/io/siddhi/extension/store/rdbms/test/osgi/CarbonDSReferenceTest.java
+++ tests/osgi-tests/src/test/java/io/siddhi/extension/store/rdbms/test/osgi/CarbonDSReferenceTest.java
@@ -18,11 +18,9 @@ package io.siddhi.extension.store.rdbms.test.osgi;
 
 import io.siddhi.extension.store.rdbms.test.osgi.util.HTTPResponseMessage;
 import io.siddhi.extension.store.rdbms.test.osgi.util.TestUtil;
-import org.ops4j.pax.exam.Configuration;
 import org.ops4j.pax.exam.ExamFactory;
-import org.ops4j.pax.exam.Option;
 import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
-import org.ops4j.pax.exam.spi.reactors.PerMethod;
+import org.ops4j.pax.exam.spi.reactors.PerClass;
 import org.ops4j.pax.exam.testng.listener.PaxExam;
 import org.testng.Assert;
 import org.testng.annotations.Listeners;
@@ -31,76 +29,47 @@ import org.wso2.carbon.container.CarbonContainerFactory;
 import org.wso2.carbon.kernel.CarbonServerInfo;
 
 import java.net.URI;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.sql.SQLException;
 import javax.inject.Inject;
-
-import static org.wso2.carbon.container.options.CarbonDistributionOption.copyFile;
 
 /**
  * SiddhiAsAPI OSGI Tests.
  */
-
 @Listeners(PaxExam.class)
-@ExamReactorStrategy(PerMethod.class)
+@ExamReactorStrategy(PerClass.class)
 @ExamFactory(CarbonContainerFactory.class)
 public class CarbonDSReferenceTest {
 
     @Inject
     private CarbonServerInfo carbonServerInfo;
 
-    @Configuration
-    public Option[] createConfiguration() {
-        return new Option[]{
-                copyCarbonDSConfigFile(),
-                copySiddhiApp()
-        };
-    }
-
-    private Option copySiddhiApp() {
-        Path carbonYmlFilePath;
-        String basedir = System.getProperty("basedir");
-        if (basedir == null) {
-            basedir = Paths.get(".").toString();
-        }
-        carbonYmlFilePath = Paths.get(basedir, "src", "test", "resources", "SiddhiApp1.siddhi");
-        return copyFile(carbonYmlFilePath, Paths.get("deployment", "siddhi-files", "SiddhiApp1.siddhi"));
-    }
-
-    private Option copyCarbonDSConfigFile() {
-        Path carbonYmlFilePath;
-        String basedir = System.getProperty("basedir");
-        if (basedir == null) {
-            basedir = Paths.get(".").toString();
-        }
-        carbonYmlFilePath = Paths.get(basedir, "src", "test", "resources", "deployment1.yaml");
-        return copyFile(carbonYmlFilePath, Paths.get("conf", "default", "deployment.yaml"));
-    }
-
     /*
     Siddhi App deployment related test cases
      */
-
     @Test
-    public void testValidDataSourceReference() throws SQLException {
-        URI baseURI = URI.create(String.format("https://%s:%d", "localhost", 9443));
-        String path = "/simulation/single";
-        String contentType = "text/plain";
-        String method = "POST";
-        String body = "{\n" +
-                "  \"siddhiAppName\": \"SiddhiApp1\",\n" +
-                "  \"streamName\": \"FooStream\",\n" +
-                "  \"timestamp\": null,\n" +
-                "  \"data\": [\n" +
-                "    \"ID001\", 148.34, 72.00\n" +
-                "  ]\n" +
+    public void testValidDataSourceReference() {
+        URI baseURI = URI.create(String.format("http://%s:%d", "localhost", 9090));
+        String path = "/stores/query";
+        String contentType = "application/json";
+        String body = "{" +
+                "  \"appName\": \"SiddhiApp1\"," +
+                "  \"query\": \"from FooTable select *;\"" +
                 "}";
-        System.setProperty("javax.net.ssl.trustStore", System.getProperty("carbon.home")
-                + "/resources/security/client-truststore.jks");
-        System.setProperty("javax.net.ssl.trustStorePassword", "wso2carbon");
-        HTTPResponseMessage httpResponseMessage = TestUtil.sendHRequest(body, baseURI, path, contentType, method,
+        HTTPResponseMessage httpResponseMessage = TestUtil.sendHRequest(body, baseURI, path, contentType,
                 true, "admin", "admin");
         Assert.assertEquals(httpResponseMessage.getResponseCode(), 200);
+    }
+
+    @Test
+    public void testInvalidDataSourceReference() {
+        URI baseURI = URI.create(String.format("http://%s:%d", "localhost", 9090));
+        String path = "/stores/query";
+        String contentType = "application/json";
+        String body = "{" +
+                "  \"appName\": \"SiddhiApp2\"," +
+                "  \"query\": \"from FooTable select *;\"" +
+                "}";
+        HTTPResponseMessage httpResponseMessage = TestUtil.sendHRequest(body, baseURI, path, contentType,
+                true, "admin", "admin");
+        Assert.assertEquals(httpResponseMessage.getResponseCode(), 500);
     }
 }
