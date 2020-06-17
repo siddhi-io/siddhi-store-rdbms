bf12c58e32ebfc0ebc0b9ccad12962e1f0218070
sahandilshan <sahandilshansg@gmail.com>
2020-06-17 14:19:13 +0530

    Add missing license headers

--- component/src/main/java/io/siddhi/extension/store/rdbms/metrics/RDBMSMetrics.java
+++ component/src/main/java/io/siddhi/extension/store/rdbms/metrics/RDBMSMetrics.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package io.siddhi.extension.store.rdbms.metrics;
 
 import com.google.common.base.Stopwatch;
@@ -42,7 +59,6 @@ public class RDBMSMetrics {
                 .counter(String.format("io.siddhi.SiddhiApps.%s.Siddhi.Store.RDBMS", siddhiAppName), Level.INFO).inc();
         RDBMS_STATUS_SERVICE_STARTED_MAP.putIfAbsent(siddhiAppName, false);
     }
-
 
     public void updateTableStatus(ExecutorService executorService, String siddhiAppName) {
         if (!RDBMS_STATUS_SERVICE_STARTED_MAP.get(siddhiAppName)) {
@@ -188,7 +204,6 @@ public class RDBMSMetrics {
                             return -1;
                         });
     }
-
 
     private synchronized void updateLastReceivedTimeMap(long lastReceivedTime) {
         if (RDBMS_LAST_RECEIVED_TIME_MAP.containsKey(rdbmsDatabase)) {
