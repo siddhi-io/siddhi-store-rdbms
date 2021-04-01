a32df4621bfd0d4eb694ca736da11f8e3af89cc5
AnuGayan <anushaj@wso2.com>
2021-04-01 18:28:19 +0530

    Allow adding null values to rdbms tables

---
+++ component/src/main/java/io/siddhi/extension/store/rdbms/config/RDBMSDataType.java
@@ -0,0 +1,45 @@
+/*
+ *  Copyright (c) 2021, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package io.siddhi.extension.store.rdbms.config;
+
+
+/**
+ * This class represents the SQL datatype and respective int values required by Siddhi RDBMS Event Tables per
+ * supported DB vendor.
+ */
+public class RDBMSDataType {
+    private String typeName;
+    private int typeValue;
+
+    public String getTypeName() {
+        return typeName;
+    }
+
+    public void setTypeName(String typeName) {
+        this.typeName = typeName;
+    }
+
+    public int getTypeValue() {
+        return typeValue;
+    }
+
+    public void setTypeValue(int typeValue) {
+        this.typeValue = typeValue;
+    }
+}
